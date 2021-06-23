2127dda97afdcdaf0f55b85adb899e87658617a4
teetarus <victortarus6@gmail.com>
2021-06-23 20:15:34 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Planets from './components/Planets'
 import People from './components/People'
-import {ReactQueryDevtools} from 'react-query/devtools'
+
 function App() {
   const [page, setPage] = useState<string>('planets')
 
@@ -23,7 +23,7 @@ function App() {
 {page==='planets'?<Planets/>:<People/>}
       </div>
       </div>
-      <ReactQueryDevtools initialIsOpen={false}/>
+      
       </>
   );
 }
